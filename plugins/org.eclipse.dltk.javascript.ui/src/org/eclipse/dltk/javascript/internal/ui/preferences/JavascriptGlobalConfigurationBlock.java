c52159ee74a83facd54e7e63848eead3126bb771
emera <emera@servoy.com>
2023-04-06 14:50:29 +0300

    SVY-17537 spike: investigate js syntax tree parsing for supporting stuff like Template Literals or Arrow Functions (clean build when preference is changed to regenerate the markers)

--- plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavascriptGlobalConfigurationBlock.java
+++ plugins/org.eclipse.dltk.javascript.ui/src/org/eclipse/dltk/javascript/internal/ui/preferences/JavascriptGlobalConfigurationBlock.java
@@ -9,6 +9,15 @@
  *******************************************************************************/
 package org.eclipse.dltk.javascript.internal.ui.preferences;
 
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.dltk.core.SourceParserUtil;
 import org.eclipse.dltk.javascript.parser.JavascriptParserPreferences;
 import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlock;
 import org.eclipse.dltk.ui.preferences.OverlayPreferenceStore;
@@ -22,6 +31,7 @@ import org.eclipse.swt.widgets.Control;
 public class JavascriptGlobalConfigurationBlock extends AbstractConfigurationBlock  {
 	private Button useES6ParserButton;
 	private JavascriptParserPreferences preferences;
+	private boolean changed = false;
 
 	public JavascriptGlobalConfigurationBlock(OverlayPreferenceStore store, PreferencePage mainPreferencePage) {
 		super(store, mainPreferencePage);
@@ -36,7 +46,7 @@ public class JavascriptGlobalConfigurationBlock extends AbstractConfigurationBlo
 	public Control createControl(Composite parent) {
 		initializeDialogUnits(parent);
 
-		Composite composite= new Composite(parent, SWT.NONE);
+		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new GridLayout());
 		
 		useES6ParserButton = new Button(composite, SWT.CHECK);
@@ -45,17 +55,36 @@ public class JavascriptGlobalConfigurationBlock extends AbstractConfigurationBlo
 		useES6ParserButton.setText(
 				"Enable the EcmaScript parser");
 		useES6ParserButton.setToolTipText(
-				"Allows support of ES6 features like arrow functions and template strings");
+				"Allows support of ES6 features like arrow functions and template strings.\nIf you change this preference, a clean build is triggered.");
 		useES6ParserButton.addListener(SWT.Selection, event -> {
 			preferences.useES6Parser(
 					useES6ParserButton.getSelection());
+			changed = !changed;
 		});
 		return composite;		
 	}
 
 	@Override
 	public void performOk() {
+		if (!changed)
+			return;
 		preferences.save();
+		changed = false;
+		final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		Job cleanJob = new Job("Clean workspace build") {
+			public IStatus run(IProgressMonitor monitor) {
+				try {
+					SourceParserUtil.clearCache();
+					workspace.build(IncrementalProjectBuilder.CLEAN_BUILD,
+							monitor);
+					return Status.OK_STATUS;
+				} catch (CoreException ex) {
+					return ex.getStatus();
+				}
+			}
+		};
+		cleanJob.setRule(workspace.getRoot());
+		cleanJob.schedule();
 		super.performOk();
 	}
 }

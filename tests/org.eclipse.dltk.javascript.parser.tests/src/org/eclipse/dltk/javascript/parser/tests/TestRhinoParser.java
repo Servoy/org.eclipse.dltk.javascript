Servoy/org.eclipse.dltk.javascript

commit 4894a137ff67f2659a75a3396a7c692fdf8ba816

SVY-19648 new es6 parser - investigate warnings which are reported with the new parser for a complex solution -various fixes to sync source end with the old parser and for docs

plugins/org.eclipse.dltk.javascript.parser/src/org/eclipse/dltk/javascript/parser/rhino/Parser.java
@@ -1186,7 +1186,17 @@ public class Parser implements IParser{
 		if (currentToken != Token.LC // assertion can be invalid in bad code
 				&& !compilerEnv.isIdeMode()) codeBug();
 		int pos = ts.getTokenBeg();
-		StatementBlock block = parent instanceof StatementBlock ? (StatementBlock) parent : new StatementBlock(getParent());
+		StatementBlock block;
+		boolean popParents = false;
+		if (parent instanceof StatementBlock) {
+			block = (StatementBlock) parent;
+		}
+		else 
+		{
+			block = new StatementBlock(getParent());
+			parents.push(block);
+			popParents = true;
+		}
 		block.setLC(pos);
 		block.setStart(pos);
 
@@ -1204,6 +1214,7 @@ public class Parser implements IParser{
 		}
 		block.setEnd(ts.getTokenEnd());
 		if (tt == Token.RC) block.setRC(ts.getTokenBeg());
+		if (popParents) parents.pop();
 		return block;
 	}
 
@@ -1475,7 +1486,7 @@ public class Parser implements IParser{
 		}
 		Statement endNode = ifFalse != null ? ifFalse : ifTrue;
 		pn.setStart(pos);
-		pn.setEnd(endNode.end());
+		pn.setEnd(getSourceEnd(endNode));
 		pn.setCondition(data.condition);
 		pn.setLP(data.lp);
 		pn.setRP(data.rp);
@@ -1486,6 +1497,15 @@ public class Parser implements IParser{
 		}
 		parents.pop();
 		return pn;
+	}
+
+	private int getSourceEnd(Statement endNode) throws IOException {
+		int end = endNode.end();
+		if (endNode instanceof ISemicolonStatement sc && sc.getSemicolonPosition() == -1)
+		{
+			end = Math.max(prevTokenStart, end);
+		}
+		return end; 
 	}
 
 	private SwitchStatement switchStatement() throws IOException {
@@ -1512,10 +1532,11 @@ public class Parser implements IParser{
 
 			boolean hasDefault = false;
 			int tt;
+			SwitchComponent comp = null;
+			ASTNode nextStmt = null;
 			switchLoop:
 				for (; ; ) {
 					tt = nextToken();
-					SwitchComponent comp = null;
 					switch (tt) {
 					case Token.RC:
 						pn.setRC(ts.getTokenBeg());
@@ -1523,6 +1544,7 @@ public class Parser implements IParser{
 						break switchLoop;
 
 					case Token.CASE:
+						adjustEnd(comp, nextStmt);
 						comp = new CaseClause(pn);
 						parents.push(comp);
 						((CaseClause) comp).setCaseKeyword(createKeyword(Token.CASE, ts.getTokenBeg()));
@@ -1536,6 +1558,7 @@ public class Parser implements IParser{
 						break;
 
 						case Token.DEFAULT:
+							adjustEnd(comp, nextStmt);
 							if (hasDefault) {
 								//reportError("msg.double.switch.default");
 								reporter.setMessage(JavaScriptParserProblems.DOUBLE_SWITCH_DEFAULT);
@@ -1572,7 +1595,7 @@ public class Parser implements IParser{
 							consumeToken();
 							continue;
 						}
-						ASTNode nextStmt = statement();
+						nextStmt = statement();
 						comp.getStatements().add((Statement) nextStmt);
 						comp.setEnd(nextStmt.end() > 0 ? nextStmt.end() : ts.getTokenEnd());
 					}
@@ -1583,6 +1606,12 @@ public class Parser implements IParser{
 			parents.pop();
 		}
 		return pn;
+	}
+
+	private void adjustEnd(SwitchComponent comp, ASTNode nextStmt) {
+		if (comp != null && nextStmt instanceof ISemicolonStatement sc && sc.getSemicolonPosition() == -1) {
+			comp.setEnd(prevTokenEnd + 1);
+		}
 	}
 
 	private WhileStatement whileLoop() throws IOException {
@@ -2507,10 +2536,8 @@ public class Parser implements IParser{
 			}
 
 			Comment jsdocNode = getAndResetJsDoc();
-			if (jsdocNode != null) 
-			{
-				((Documentable)variableStatement).setDocumentation(jsdocNode);
-			}
+			((Documentable)variableStatement).setDocumentation(jsdocNode != null ? jsdocNode : varjsdocNode);
+			if (name != null) name.setDocumentation(jsdocNode != null ? jsdocNode : varjsdocNode);
 			VariableDeclaration variableDeclaration = new VariableDeclaration(variableStatement); 
 			parents.push(variableDeclaration);
 			Expression init = null;
@@ -3479,7 +3506,11 @@ public class Parser implements IParser{
 			result.setDocumentation(pn.getDocumentation());
 		}
 		else {
-			result.setDocumentation(getAndResetJsDoc());
+			Comment comment = getAndResetJsDoc();
+			result.setDocumentation(comment);
+			if (ref.getDocumentation() == null && ref instanceof Documentable doc) {
+				doc.setDocumentation(comment);
+			}
 		}
 		result.setStart(pn.start());
 		result.setEnd(ref.end());
@@ -3646,7 +3677,12 @@ public class Parser implements IParser{
 
 		case Token.NAME:
 			consumeToken();
-			return name(ttFlagged, tt);
+			Expression name = name(ttFlagged, tt);
+			if (name instanceof Documentable doc && name.getDocumentation() == null && currentJsDocComment != null
+					&& currentJsDocComment.sourceStart() < name.start()) {
+				doc.setDocumentation(getAndResetJsDoc());
+			}
+			return name;
 
 		case Token.NUMBER:
 		case Token.BIGINT:

tests/org.eclipse.dltk.javascript.parser.tests/src/org/eclipse/dltk/javascript/parser/tests/TestRhinoParser.java
@@ -109,7 +109,7 @@ public class TestRhinoParser {
 							.getDocumentation();
 					if (documentation1 != null) {
 						if (documentation2 == null) {
-							fail("the node fails mising documentation for  " + node2 + " that should be "  + node1 + "\nstack: " + stack );
+						 	fail("the node fails mising documentation for  " + node2 + " that should be "  + node1 + "\nstack: " + stack );
 							return false;
 						}
 						if (!equalsJSNode(documentation1, documentation2,
@@ -118,6 +118,10 @@ public class TestRhinoParser {
 							return false;
 						}
 					}
+//					else if (documentation2 != null && documentation1 == null)
+//					{
+//						System.out.println(node1 +" is missing documentation in the old parser.");
+//					}
 				}
 				if (node1 instanceof StatementBlock sb1) {
 					if (node2 instanceof StatementBlock sb2) {
@@ -131,14 +135,21 @@ public class TestRhinoParser {
 						return false;
 					}
 				}
-				if (node1.toString().equals(node2.toString())
-						&& node1.sourceStart() == node2.sourceStart()
-						&& node1.sourceEnd() == node2.sourceEnd())
+				if (!node1.toString().equals(node2.toString()))
 				{
-					return true;
+					fail("nodes are not equal in source toString or don't have the same children, stack: " + stack + "\nnode1:\n" + node1 + "\nnode2:\n" + node2);
 				}
+				if (node1.sourceStart() != node2.sourceStart())
+				{
+					fail("nodes are not equal in source start "+node1.sourceStart() +" !=" + node2.sourceStart()+", stack: " + stack + "\nnode1:\n" + node1 + "\nnode2:\n" + node2);
+				}
+				if (node1.sourceEnd() != node2.sourceEnd())
+				{
+					fail("nodes are not equal in source end "+node1.sourceEnd() +" !=" + node2.sourceEnd()+", stack: " + stack + "\nnode1:\n" + node1 + "\nnode2:\n" + node2);
+				}
+				return true;
 			}
-			fail("nodes are not equal in source end/start or don't have the same childre, stack: " + stack + "\nnode1:\n" + node1 + "\nnode2:\n" + node2);
+			fail("nodes adon't have the same children, stack: " + stack + "\nnode1:\n" + node1 + "\nnode2:\n" + node2);
 			return false;
 		} finally {
 			stack.pop();
@@ -2109,6 +2120,73 @@ public class TestRhinoParser {
 		
 		Script scriptv4 = getScriptv4(source);
 		assertNotNull(scriptv4);
+		assertTrue(equalsJSNode(script, scriptv4, new ArrayDeque<>()));
+	}
+	
+	@Test
+	public void testBreakNoSemiColon() {
+		String source = "			switch (fieldType) {\r\n"
+				+ "				case JSColumn.DATETIME:\r\n"
+				+ "					columnValue = row[attr.valueDate];\r\n"
+				+ "					break\r\n" //break is not followed by ;
+				+ "				default:\r\n"
+				+ "					columnValue = row[attr.value];\r\n"
+				+ "					break;\r\n"
+				+ "			}\r\n";
+		Script script = getScript(source);	
+		assertNotNull(script);
+
+		Script scriptv4 = getScriptv4(source);
+		assertNotNull(scriptv4);
+		assertTrue(equalsJSNode(script, scriptv4, new ArrayDeque<>()));
+	}
+	
+	@Test
+	public void testReturnNoSemiColonThenComment() {
+		String source = "if (true) return []\r\n"
+				+ "	/** @type {Array} */\r\n"
+				+ "	var a1 = arr.slice(0) // Start with a copy\r\n";
+		Script script = getScript(source);	
+		assertNotNull(script);
+
+		Script scriptv4 = getScriptv4(source);
+		assertNotNull(scriptv4);
+		assertTrue(equalsJSNode(script, scriptv4, new ArrayDeque<>()));
+	}
+
+	@Test		
+	public void testReturnDoc() {
+		String source = "/** @type {JSDataSet<{\r\n"
+				+ "     *  unique_identifier: String\r\n"
+				+ "     *  attribute_id: Number,\r\n"
+				+ "     *  }>}\r\n"
+				+ "     */\r\n"
+				+ "	return databaseManager.getDataSetByQuery(select, -1)";
+		Script script = getScript(source);	
+		assertNotNull(script);
+
+		Script scriptv4 = getScriptv4(source);
+		assertNotNull(scriptv4);
+		assertEquals(script.getComments().get(0).getText(), scriptv4.getComments().get(0).getText());
+		assertEquals(script.getComments().get(0).sourceStart(), scriptv4.getComments().get(0).sourceStart());
+		assertEquals(script.getComments().get(0).sourceEnd(), scriptv4.getComments().get(0).sourceEnd());
+		assertTrue(equalsJSNode(script, scriptv4, new ArrayDeque<>()));
+	}
+	
+	@Test		
+	public void testIdentifierDoc() {
+		String source =  " /** @type {JsRecord} */\r\n"
+				+ "		this.record = record\r\n"
+				+ "		/** @type {RegExp} */\r\n"
+				+ "		some_expr = 1\r\n";
+		Script script = getScript(source);	
+		assertNotNull(script);
+
+		Script scriptv4 = getScriptv4(source);
+		assertNotNull(scriptv4);
+		assertEquals(script.getComments().get(0).getText(), scriptv4.getComments().get(0).getText());
+		assertEquals(script.getComments().get(0).sourceStart(), scriptv4.getComments().get(0).sourceStart());
+		assertEquals(script.getComments().get(0).sourceEnd(), scriptv4.getComments().get(0).sourceEnd());
 		assertTrue(equalsJSNode(script, scriptv4, new ArrayDeque<>()));
 	}
 }

plugins/org.eclipse.dltk.javascript.parser/.classpath
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <classpath>
-	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.8"/>
+	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-16"/>
 	<classpathentry kind="con" path="org.eclipse.pde.core.requiredPlugins"/>
 	<classpathentry kind="src" path="src"/>
 	<classpathentry kind="output" path="bin"/>

plugins/org.eclipse.dltk.javascript.parser/.settings/org.eclipse.jdt.core.prefs
@@ -1,8 +1,8 @@
 eclipse.preferences.version=1
 org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
-org.eclipse.jdt.core.compiler.codegen.targetPlatform=1.8
+org.eclipse.jdt.core.compiler.codegen.targetPlatform=16
 org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve
-org.eclipse.jdt.core.compiler.compliance=1.8
+org.eclipse.jdt.core.compiler.compliance=16
 org.eclipse.jdt.core.compiler.debug.lineNumber=generate
 org.eclipse.jdt.core.compiler.debug.localVariable=generate
 org.eclipse.jdt.core.compiler.debug.sourceFile=generate
@@ -16,6 +16,7 @@ org.eclipse.jdt.core.compiler.problem.deprecationInDeprecatedCode=disabled
 org.eclipse.jdt.core.compiler.problem.deprecationWhenOverridingDeprecatedMethod=disabled
 org.eclipse.jdt.core.compiler.problem.discouragedReference=warning
 org.eclipse.jdt.core.compiler.problem.emptyStatement=ignore
+org.eclipse.jdt.core.compiler.problem.enablePreviewFeatures=disabled
 org.eclipse.jdt.core.compiler.problem.enumIdentifier=error
 org.eclipse.jdt.core.compiler.problem.fallthroughCase=ignore
 org.eclipse.jdt.core.compiler.problem.fatalOptionalError=disabled
@@ -78,8 +79,8 @@ org.eclipse.jdt.core.compiler.problem.unusedParameterWhenOverridingConcrete=disa
 org.eclipse.jdt.core.compiler.problem.unusedPrivateMember=warning
 org.eclipse.jdt.core.compiler.problem.unusedWarningToken=warning
 org.eclipse.jdt.core.compiler.problem.varargsArgumentNeedCast=warning
-org.eclipse.jdt.core.compiler.release=disabled
-org.eclipse.jdt.core.compiler.source=1.8
+org.eclipse.jdt.core.compiler.release=enabled
+org.eclipse.jdt.core.compiler.source=16
 org.eclipse.jdt.core.formatter.align_type_members_on_columns=false
 org.eclipse.jdt.core.formatter.alignment_for_additive_operator=16
 org.eclipse.jdt.core.formatter.alignment_for_arguments_in_allocation_expression=16

commit f766342e1de8d3a5ed484c75cc7a27bf4f35fb08

SVY-21422 fix double JSDoc generation on var without trailing semicolon [ai]
When a var declaration relies on ASI (no trailing ';') and its
initializer
ends in a property access, the ASI look-ahead scans the next statement's
JSDoc into the shared currentJsDocComment latch. The else-branch of
createPropertyExpression then grabbed it unconditionally and attached it
to
the property, leaking the next declaration's JSDoc.

Guard the else-branch attachment with
currentJsDocComment.sourceStart() < pn.start(), matching the guard
already
used in the sibling Documentable branch. Regression was introduced by
4894a137. Added 3 tests to TestRhinoParser.

plugins/org.eclipse.dltk.javascript.parser/src/org/eclipse/dltk/javascript/parser/rhino/Parser.java
@@ -3867,10 +3867,17 @@ public class Parser implements IParser{
 			result.setDocumentation(pn.getDocumentation());
 		}
 		else {
-			Comment comment = getAndResetJsDoc();
-			result.setDocumentation(comment);
-			if (ref.getDocumentation() == null && ref instanceof Documentable doc) {
-				doc.setDocumentation(comment);
+			// Only attach a pending JSDoc if it appears before this property
+			// expression in the source. Without a trailing ';' the ASI
+			// look-ahead may have already scanned the JSDoc that belongs to
+			// the next statement into currentJsDocComment; that comment must
+			// not leak onto this property (SVY-21422).
+			if (currentJsDocComment != null && currentJsDocComment.sourceStart() < pn.start()) {
+				Comment comment = getAndResetJsDoc();
+				result.setDocumentation(comment);
+				if (ref.getDocumentation() == null && ref instanceof Documentable doc) {
+					doc.setDocumentation(comment);
+				}
 			}
 		}
 		result.setStart(pn.start());

tests/org.eclipse.dltk.javascript.parser.tests/src/org/eclipse/dltk/javascript/parser/tests/TestRhinoParser.java
@@ -8498,5 +8498,124 @@ public class TestRhinoParser {
 				arrow.getArguments().size());
 	}
 
+	// SVY-21422: a var declaration terminated by ASI (no trailing ';') whose
+	// initializer ends in a property access must not leak its following JSDoc
+	// onto the next declaration. Regression introduced by 4894a137.
+	@Test
+	public void testDoubleJsdoc_missingSemicolon_doesNotLeakToNextVar() {
+		String source = "/**\r\n"
+				+ " * @properties={typeid:35,uuid:\"70CFCBF9-573E-46AF-8216-F99B5CB7D65F\",variableType:-4}\r\n"
+				+ " */\r\n"
+				+ "var pdfBytes = solutionModel.getMedia('blank.pdf').bytes\r\n"
+				+ "\r\n"
+				+ "/**\r\n"
+				+ " * @type {String}\r\n"
+				+ " *\r\n"
+				+ " * @properties={typeid:35,uuid:\"7D4D9708-081F-4BEB-9AF2-654D8618D030\"}\r\n"
+				+ " */\r\n"
+				+ "var _titleName";
+		Script scriptv4 = getScriptv4(source);
+		assertNotNull(scriptv4);
+		assertEquals(2, scriptv4.getStatements().size());
+
+		VariableStatement first = (VariableStatement) ((VoidExpression) scriptv4
+				.getStatements().get(0)).getExpression();
+		VariableStatement second = (VariableStatement) ((VoidExpression) scriptv4
+				.getStatements().get(1)).getExpression();
+
+		assertNotNull("first var must keep its own JSDoc", first.getDocumentation());
+		assertTrue("first var doc must be the first block",
+				first.getDocumentation().getText().contains("70CFCBF9-573E-46AF-8216-F99B5CB7D65F"));
+		assertFalse("first var doc must NOT contain the second block (no leak)",
+				first.getDocumentation().getText().contains("7D4D9708-081F-4BEB-9AF2-654D8618D030"));
+
+		assertNotNull("second var must get its own JSDoc", second.getDocumentation());
+		assertTrue("second var doc must be the second block",
+				second.getDocumentation().getText().contains("7D4D9708-081F-4BEB-9AF2-654D8618D030"));
+		assertFalse("second var doc must NOT contain the first block",
+				second.getDocumentation().getText().contains("70CFCBF9-573E-46AF-8216-F99B5CB7D65F"));
+
+		// The property expression that ends the first initializer must not have
+		// captured the second statement's JSDoc.
+		VariableDeclaration firstDecl = first.getVariables().get(0);
+		Expression init = firstDecl.getInitializer();
+		assertTrue(init instanceof PropertyExpression);
+		assertFalse("property expression must not leak the next JSDoc",
+				init.getDocumentation() != null && init.getDocumentation().getText()
+						.contains("7D4D9708-081F-4BEB-9AF2-654D8618D030"));
+	}
+
+	// SVY-21422: the explicit ';' variant already worked; guard against a
+	// regression that would break it.
+	@Test
+	public void testDoubleJsdoc_withSemicolon_stillCorrect() {
+		String source = "/**\r\n"
+				+ " * @properties={typeid:35,uuid:\"70CFCBF9-573E-46AF-8216-F99B5CB7D65F\",variableType:-4}\r\n"
+				+ " */\r\n"
+				+ "var pdfBytes = solutionModel.getMedia('blank.pdf').bytes;\r\n"
+				+ "\r\n"
+				+ "/**\r\n"
+				+ " * @type {String}\r\n"
+				+ " *\r\n"
+				+ " * @properties={typeid:35,uuid:\"7D4D9708-081F-4BEB-9AF2-654D8618D030\"}\r\n"
+				+ " */\r\n"
+				+ "var _titleName;";
+		Script scriptv4 = getScriptv4(source);
+		assertNotNull(scriptv4);
+		assertEquals(2, scriptv4.getStatements().size());
+
+		VariableStatement first = (VariableStatement) ((VoidExpression) scriptv4
+				.getStatements().get(0)).getExpression();
+		VariableStatement second = (VariableStatement) ((VoidExpression) scriptv4
+				.getStatements().get(1)).getExpression();
+
+		assertNotNull(first.getDocumentation());
+		assertTrue(first.getDocumentation().getText()
+				.contains("70CFCBF9-573E-46AF-8216-F99B5CB7D65F"));
+		assertFalse(first.getDocumentation().getText()
+				.contains("7D4D9708-081F-4BEB-9AF2-654D8618D030"));
+
+		assertNotNull(second.getDocumentation());
+		assertTrue(second.getDocumentation().getText()
+				.contains("7D4D9708-081F-4BEB-9AF2-654D8618D030"));
+	}
+
+	// SVY-21422: minimal isolation of the property-expression path. The
+	// initializer must end in a property access whose object is NOT itself
+	// Documentable (a call expression), so createPropertyExpression takes the
+	// else-branch that previously leaked the next statement's JSDoc.
+	@Test
+	public void testDoubleJsdoc_singleVarNoSemicolon_propertyAccessInitializer() {
+		String source = "/** FIRSTDOC */\r\n"
+				+ "var x = obj.get().prop\r\n"
+				+ "/** SECONDDOC */\r\n"
+				+ "var y";
+		Script scriptv4 = getScriptv4(source);
+		assertNotNull(scriptv4);
+		assertEquals(2, scriptv4.getStatements().size());
+
+		VariableStatement first = (VariableStatement) ((VoidExpression) scriptv4
+				.getStatements().get(0)).getExpression();
+		VariableStatement second = (VariableStatement) ((VoidExpression) scriptv4
+				.getStatements().get(1)).getExpression();
+
+		assertNotNull(first.getDocumentation());
+		assertTrue(first.getDocumentation().getText().contains("FIRSTDOC"));
+		assertFalse("no leak of SECONDDOC onto first var",
+				first.getDocumentation().getText().contains("SECONDDOC"));
+
+		assertNotNull(second.getDocumentation());
+		assertTrue(second.getDocumentation().getText().contains("SECONDDOC"));
+		assertFalse(second.getDocumentation().getText().contains("FIRSTDOC"));
+
+		// The property-access initializer of the first var must not have
+		// captured the second statement's JSDoc.
+		Expression init = first.getVariables().get(0).getInitializer();
+		assertTrue(init instanceof PropertyExpression);
+		assertFalse("property expression must not leak SECONDDOC",
+				init.getDocumentation() != null
+						&& init.getDocumentation().getText().contains("SECONDDOC"));
+	}
+
 
 }